e6c70cd770a7db085adf59e3c74e5f2e563fa899
MollyERG <mollymaloun@gmail.com>
2024-04-08 20:14:50 +0300

    Remove garbage usings

--- EasyESS/Services/MessageBroker/Scheduler/SchedulerConfig.cs
+++ EasyESS/Services/MessageBroker/Scheduler/SchedulerConfig.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace EasyESS.Services.MessageBroker.Scheduler
+﻿namespace EasyESS.Services.MessageBroker.Scheduler
 {
     internal class SchedulerConfig
     {
