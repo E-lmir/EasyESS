af809a363ad8cbc8ec71bfaa1533c55d65255342
MollyERG <mollymaloun@gmail.com>
2024-04-08 17:03:29 +0300

    Add ServiceProvider

---
+++ EasyESS/Services/ServiceProvider.cs
@@ -0,0 +1,32 @@
+﻿using EasyESS.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyESS.Services
+{
+    public static class ServiceProvider
+    {
+        public static void Install<TService>(InstallationInfo info, ILogger logger = null) where TService : IService, new()
+        {
+            var service = new TService();
+            var TType = typeof(TService).ToString().Split('.').LastOrDefault();
+            service.ExtractFiles(info);
+            service.FillConfig(info);
+            logger?.Log("1");
+            if (service is IDataAccessable dataAccessable)
+                dataAccessable.CreateDb(info);
+
+            if (service is IHostable hostable)
+                hostable.AddToIIS(info);
+
+            if (service is IRegistrable registrable)
+                registrable.Register(info);
+
+            if (service is IConfigurable configurable)
+                configurable.Configure(info);
+        }
+    }
+}
